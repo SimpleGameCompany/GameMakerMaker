c0545dbbeb5cff058002544cb9b51b6beffe4c4f
pabloski300 <pabloski300@hotmail.com>
2018-12-14 11:32:48 +0100

    Grid Terminado

--- Desarrollo/GameMakerMaker/Assets/GridController.cs
+++ Desarrollo/GameMakerMaker/Assets/GridController.cs
@@ -5,81 +5,136 @@ using UnityEngine;
 
 public class GridController : EditorWindow
 {
+    static Vector2 scrollPos;
 
-    static int x;
-    static int y;
+    static int x = 1;
+    static int y = 1;
 
     static int oldX;
-    static int oldY;
+    static int oldY = 1;
 
     static Material levelFloor = null;
     static Editor levelFloorEditor = null;
 
     static List<List<GameObject>> level = new List<List<GameObject>>();
-    static List<List<Editor>> levelEditors = new List<List<Editor>>();
+    static List<List<GameObject>> levelObjects = new List<List<GameObject>>();
+    static GameObject floor;
+    static GameObject floorPref;
 
     [MenuItem("GameMakerMaker/GridController")]
     static void Init()
     {
         GridController window = (GridController)EditorWindow.GetWindow(typeof(GridController));
         window.Show();
+        //GridController windowScroll = (GridController)EditorWindow.GetWindow(typeof(GridController), true);
+        //windowScroll.Show();
     }
 
 
     static void ShowWindow()
     {
-        GetWindow<GridController>("LevelEditor");
+        GetWindow<GridController>("GridController");
     }
 
     private void OnGUI()
     {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
+        #region Dimensiones
         GUILayout.Label("Dimensiones", EditorStyles.boldLabel);
 
         GUILayout.BeginHorizontal();
-        x = EditorGUILayout.IntField(x, GUILayout.ExpandWidth(false));
-        y = EditorGUILayout.IntField(y, GUILayout.ExpandWidth(false));
-        GUILayout.EndHorizontal();
 
+        if (GUILayout.Button("-", GUILayout.ExpandWidth(false), GUILayout.MaxWidth(20f)) && x > 1)
+        {
+            x -= 2;
+        }
+        GUI.enabled = false;
+        x = EditorGUILayout.IntField(x, GUILayout.ExpandWidth(false), GUILayout.MaxWidth(20f));
+        GUI.enabled = true;
+        if (GUILayout.Button("+", GUILayout.ExpandWidth(false), GUILayout.MaxWidth(20f)))
+        {
+            x += 2;
+        }
+
+        if (GUILayout.Button("-", GUILayout.ExpandWidth(false), GUILayout.MaxWidth(20f)) && y > 1)
+        {
+            y -= 2;
+        }
+        GUI.enabled = false;
+        y = EditorGUILayout.IntField(y, GUILayout.ExpandWidth(false), GUILayout.MaxWidth(20f));
+        GUI.enabled = true;
+        if (GUILayout.Button("+", GUILayout.ExpandWidth(false), GUILayout.MaxWidth(20f)))
+        {
+            y += 2;
+        }
+
+        GUILayout.EndHorizontal();
+        #endregion
+
+        #region Suelo
         GUILayout.Label("Suelo", EditorStyles.boldLabel);
 
         GUILayout.BeginHorizontal();
-        GUILayout.BeginVertical(GUILayout.ExpandWidth(false), GUILayout.MaxWidth(320f));
-        levelFloor = (Material)EditorGUILayout.ObjectField(levelFloor, typeof(Material), false, GUILayout.ExpandWidth(false), GUILayout.MaxWidth(160f));
-        if (levelFloor != null)
+        GUILayout.BeginVertical();
+        EditorGUI.BeginChangeCheck();
+        floorPref = (GameObject)EditorGUILayout.ObjectField(floorPref, typeof(GameObject), false, GUILayout.ExpandWidth(false), GUILayout.MinWidth(160f), GUILayout.MaxWidth(160f));
+        if (EditorGUI.EndChangeCheck())
         {
-            if (levelFloorEditor == null)
-                levelFloorEditor = Editor.CreateEditor(levelFloor);
-
-            levelFloorEditor.OnPreviewGUI(GUILayoutUtility.GetRect(0, 0, GUILayout.ExpandWidth(false), GUILayout.MaxHeight(100f), GUILayout.MaxWidth(130f)), null);
+            if (floorPref != null)
+            {
+                AddFloor(floorPref);
+            }
+            else
+            {
+                RemoveFloor();
+            }
+        }
+        EditorGUI.BeginChangeCheck();
+        levelFloor = (Material)EditorGUILayout.ObjectField(levelFloor, typeof(Material), false, GUILayout.ExpandWidth(false), GUILayout.MinWidth(160f), GUILayout.MaxWidth(160f));
+        if (EditorGUI.EndChangeCheck())
+        {
+            if (levelFloor != null)
+            {
+                ChangeMaterial(levelFloor);
+            }
+            else
+            {
+                RemoveMaterial();
+            }
         }
         GUILayout.EndVertical();
         GUILayout.EndHorizontal();
+        #endregion
 
         ChangeGrid();
 
-
+        #region Grid
         GUILayout.Label("Level", EditorStyles.boldLabel);
         for (int j = 0; j < y; j++)
         {
             GUILayout.BeginHorizontal(GUILayout.ExpandWidth(false), GUILayout.MaxWidth(320f));
             for (int i = 0; i < x; i++)
             {
-                GUILayout.BeginVertical(GUILayout.ExpandWidth(false), GUILayout.MaxWidth(320f));
-                level[i][j] = (GameObject)EditorGUILayout.ObjectField(level[i][j], typeof(GameObject), false, GUILayout.ExpandWidth(false), GUILayout.MaxWidth(160f));
-
-                if (level[i][j] != null)
+                EditorGUI.BeginChangeCheck();
+                level[i][j] = (GameObject)EditorGUILayout.ObjectField(level[i][j], typeof(GameObject), false, GUILayout.ExpandWidth(false), GUILayout.MaxWidth(60f),GUILayout.MinWidth(60f), GUILayout.MaxHeight(40f), GUILayout.MinHeight(40f));
+                if (EditorGUI.EndChangeCheck())
                 {
-                    
-                    if (levelEditors[i][j] == null)
-                        levelEditors[i][j] = Editor.CreateEditor(level[i][j]);
-
-                    levelEditors[i][j].OnPreviewGUI(GUILayoutUtility.GetRect(0,0,GUILayout.ExpandWidth(false), GUILayout.MaxHeight(100f), GUILayout.MaxWidth(130f)),null);
+                    if(level[i][j] == null)
+                    {
+                        RemoveObject(i,j);
+                    }
+                    else
+                    {
+                        AddObject(i, j, level[i][j]);
+                    }
                 }
-                GUILayout.EndVertical();
-
             }
             GUILayout.EndHorizontal();
         }
+        #endregion
+
+        EditorGUILayout.EndScrollView();
     }
 
     void ChangeGrid()
@@ -90,13 +145,14 @@ public class GridController : EditorWindow
             {
 
                 level.Add(new List<GameObject>());
-                levelEditors.Add(new List<Editor>());
+                levelObjects.Add(new List<GameObject>());
                 for (int j = 0; j < y; j++)
                 {
                     level[i].Add(null);
-                    levelEditors[i].Add(null);
+                    levelObjects[i].Add(null);
                 }
             }
+            Regenerate(oldX, oldY);
             oldX = x;
         }
         else if (x < oldX)
@@ -106,24 +162,27 @@ public class GridController : EditorWindow
                 for (int j = 0; j < y; j++)
                 {
                     level[i - 1].RemoveAt(0);
-                    levelEditors[i - 1].RemoveAt(0);
+                    RemoveObject(i-1,0);
+                    levelObjects[i - 1].RemoveAt(0);
                 }
                 level.RemoveAt(i - 1);
-                levelEditors.RemoveAt(i - 1);
+                levelObjects.RemoveAt(i - 1);
             }
+            Regenerate(x, y);
             oldX = x;
         }
 
-        if (y > oldY)
+        if (y > oldY && y > 1)
         {
             for (int i = 0; i < x; i++)
             {
                 for (int j = oldY; j < y; j++)
                 {
                     level[i].Add(null);
-                    levelEditors[i].Add(null);
+                    levelObjects[i].Add(null);
                 }
             }
+            Regenerate(oldX, oldY);
             oldY = y;
         }
         else if (y < oldY)
@@ -133,10 +192,71 @@ public class GridController : EditorWindow
                 for (int j = oldY; j > y; j--)
                 {
                     level[i].RemoveAt(j - 1);
-                    levelEditors[i].RemoveAt(j - 1);
+                    RemoveObject(i, j - 1);
+                    levelObjects[i].RemoveAt(j - 1);
                 }
             }
+            Regenerate(x, y);
             oldY = y;
         }
+    }
+
+    void AddObject(int posX, int posY, GameObject prefab)
+    {
+        DestroyImmediate(levelObjects[posX][posY]);
+        levelObjects[posX][posY] = Instantiate(prefab, new Vector3(posX-(x/2), 1, -1*(posY-(y/2))), prefab.transform.rotation);
+    }
+
+    void RemoveObject(int x, int y)
+    {
+        DestroyImmediate(levelObjects[x][y]);
+    }
+
+    void Regenerate(int posX, int posY)
+    {
+        for(int i= 0; i< posX; i++)
+        {
+            for (int j = 0; j < posY; j++)
+            {
+                if (level[i][j] != null)
+                {
+                    DestroyImmediate(levelObjects[i][j]);
+                    levelObjects[i][j] = Instantiate(level[i][j], new Vector3(i - (x / 2), 1, -1*(j - (y / 2))), level[i][j].transform.rotation);
+                }
+            }
+        }
+        if (floor != null)
+        {
+            floor.transform.localPosition = new Vector3(0, 0, 0);
+            floor.transform.localScale = new Vector3(x, 1, y);
+        }
+    }
+
+    void ChangeMaterial(Material mat)
+    {
+        if (floor != null)
+        {
+            floor.GetComponent<MeshRenderer>().material = mat;
+        }
+    }
+
+    void RemoveMaterial()
+    {
+        if (floor != null)
+        {
+            floor.GetComponent<MeshRenderer>().material = null;
+        }
+    }
+
+    void AddFloor(GameObject fp)
+    {
+        DestroyImmediate(floor);
+        floor = Instantiate(fp, new Vector3(0,0,0), Quaternion.identity);
+        floor.transform.localScale = new Vector3(x, 1, y);
+    }
+
+    void RemoveFloor()
+    {
+        DestroyImmediate(floor);
     }
 }
