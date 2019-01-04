263ea0ca2a189c8c87fb2b76c8192a362310e5b4
luk224 <luking1995@gmail.com>
2019-01-04 14:06:10 +0100

    Cintas nuevas con forma de oruga

--- Desarrollo/GameMakerMaker/Assets/Script/Game/Managers/MoverCintas.cs
+++ Desarrollo/GameMakerMaker/Assets/Script/Game/Managers/MoverCintas.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class MoverCintas : MonoBehaviour {
 
     public List<Material> cintas;
+    
     public float velX =0f;
     public float velY =0.1f;
+    public List<float> offsetsY = new List<float>(0);//para poder añadir un offset a cada material.
     float offsetX = 0;
     float offsetY = 0;
     public bool setTo0 = false;
+    private GameObject[] rodillos;
 	// Use this for initialization
 	void Start () {
+        rodillos = GameObject.FindGameObjectsWithTag("Rodillo");
 		
 	}
 	
@@ -22,6 +26,8 @@ public class MoverCintas : MonoBehaviour {
          offsetY += (Time.deltaTime * velY);
         offsetX %= 1;
         offsetY %= 1;
+
+        int i = 0;//Index pa el forEach
         foreach (Material c in cintas)
         {
             if (setTo0)
@@ -30,9 +36,15 @@ public class MoverCintas : MonoBehaviour {
             }
             else
             {
-                c.mainTextureOffset = new Vector2(offsetX, offsetY);
+                c.mainTextureOffset = new Vector2(offsetX, offsetY+offsetsY[i]);
             }
+            i++;
             
+        }
+        
+        foreach(GameObject rodillo in rodillos)
+        {
+            rodillo.transform.Rotate(new Vector3(0.0f, 0.0f, velY * 2));
         }
 		
 	}
