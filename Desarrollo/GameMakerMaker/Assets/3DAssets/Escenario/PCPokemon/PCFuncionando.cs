5e66bc52482e0ff62316fe7407425c8ebc9b5dcf
luk224 <luking1995@gmail.com>
2019-01-07 13:14:15 +0100

    Prueba de script para PC

--- Desarrollo/GameMakerMaker/Assets/3DAssets/Escenario/PCPokemon/PCFuncionando.cs
+++ Desarrollo/GameMakerMaker/Assets/3DAssets/Escenario/PCPokemon/PCFuncionando.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class PCFuncionando : MonoBehaviour {
 
+    public List<Material> Mats_Funcionamiento;
+    public Material Mat_Apagado;
+    private int currentMaterial = 0;
 	// Use this for initialization
 	void Start () {
-		
+        encenderPC(0.15f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    public void encenderPC(float intervalo)
+    {
+        InvokeRepeating("nextMaterial", 0.1f,intervalo);        
+    }
+    public void pararPC()
+    {
+        GetComponent<Renderer>().material = Mat_Apagado;
+    }
+    private void nextMaterial()
+    {
+        if (currentMaterial >= Mats_Funcionamiento.Count)
+            currentMaterial = 0;
+        GetComponent<Renderer>().material = Mats_Funcionamiento[currentMaterial];
+        currentMaterial++;
+        Debug.Log(currentMaterial);
+        
+
+
+    }
 }
