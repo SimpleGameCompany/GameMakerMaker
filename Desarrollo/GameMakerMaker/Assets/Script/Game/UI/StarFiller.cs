2eded472786df1f8af37718155ae3fa98e7e5023
pabloski300 <pabloski300@hotmail.com>
2019-01-08 22:32:14 +0100

    Texto de puntuaciones añadido

--- Desarrollo/GameMakerMaker/Assets/Script/Game/UI/StarFiller.cs
+++ Desarrollo/GameMakerMaker/Assets/Script/Game/UI/StarFiller.cs
@@ -2,30 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class StarFiller : MonoBehaviour {
 
     public Sprite FullStar;
     private Image[] stars;
     public int finalScore;
+
+    public TextMeshProUGUI propsBText, propsMText, propsDText, hornosText, papeleraText, tuberiasText;
+    public GameObject[] texts;
+
+    private WaitForSeconds wait;
+
 	// Use this for initialization
 	void Start () {
+        wait = new WaitForSeconds(0.5f);
         stars = GetComponentsInChildren<Image>();
         foreach(var e in stars)
         {
             e.gameObject.SetActive(false);
         }
-        
-	}
+        foreach (var e in texts)
+        {
+            e.gameObject.SetActive(false);
+        }
+    }
 	
     public IEnumerator FillStars(float points,float maxPoints)
     {
+        yield return wait;
+        if (ScoreController.Instance.propComplejo > 0)
+        {
+            propsDText.text = ScoreController.Instance.propComplejo.ToString();
+            texts[0].SetActive(true);
+        }
+        yield return wait;
+        if (ScoreController.Instance.propNormal > 0)
+        {
+            propsMText.text = ScoreController.Instance.propNormal.ToString();
+            texts[1].SetActive(true);
+        }
+        yield return wait;
+        if (ScoreController.Instance.propBasico > 0)
+        {
+            propsBText.text = ScoreController.Instance.propBasico.ToString();
+            texts[2].SetActive(true);
+        }
+        yield return wait;
+        if (ScoreController.Instance.hornos > 0)
+        {
+            hornosText.text = ScoreController.Instance.hornos.ToString();
+            texts[3].SetActive(true);
+        }
+        yield return wait;
+        if (ScoreController.Instance.entregas > 0)
+        {
+            tuberiasText.text = ScoreController.Instance.entregas.ToString();
+            texts[4].SetActive(true);
+        }
+        yield return wait;
+        if (ScoreController.Instance.papelera > 0)
+        {
+            papeleraText.text = ScoreController.Instance.papelera.ToString();
+            texts[5].SetActive(true);
+        }
+
+
+
         finalScore = 0;
         maxPoints = maxPoints == 0 ? points : maxPoints;
         float completed = (points) / maxPoints;
         finalScore = Mathf.FloorToInt(completed * 3);
         finalScore = Mathf.Max(1, finalScore);
-        for(int i = 0; i < 3; i++)
+        yield return wait;
+        for (int i = 0; i < 3; i++)
         {
             Image star = stars[i];
             star.gameObject.SetActive(true);
@@ -41,10 +92,12 @@ public class StarFiller : MonoBehaviour {
                     yield return null;
                 }
 
-                yield return new WaitForSeconds(0.5f);
+                yield return wait;
             }
 
         }
+
+
         GameObject button = GameObject.FindGameObjectWithTag(Constantes.TAG_WIN_BUTTONS);
         foreach(var but in button.GetComponentsInChildren<Button>())
         {
