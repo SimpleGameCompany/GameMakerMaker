37faac862a119b1278e4d6c8c56d4e59a6140a52
Jodacam <Jodacam2@gmail.com>
2019-01-09 19:33:42 +0100

    Cambiadas un monton de cosas

---
+++ Desarrollo/GameMakerMaker/Assets/Script/Game/Tutorial/BookTutorial.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Polyglot;
+
+public class BookTutorial : BookBehaviour
+{
+    public LocalizedTextMeshProUGUI text;
+    public Light l;
+    private void Awake()
+    {
+        l = transform.parent.GetComponentInChildren<Light>();
+        l.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
+    }
+
+
+    public override void PostAction(PlayerController player)
+    {
+        TexturizadorTutorial.instance.text.gameObject.SetActive(true);
+        TexturizadorTutorial.instance.text.Key = Constantes.KEY_OVEN_GOTO;
+        TexturizadorTutorial.instance.GetComponentInChildren<Light>(true).gameObject.SetActive(true);
+        text.gameObject.SetActive(false);
+        l.gameObject.SetActive(false);
+        base.PostAction(player);
+    }
+}
